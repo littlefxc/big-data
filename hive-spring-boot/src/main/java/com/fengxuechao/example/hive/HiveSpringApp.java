dfb43bf7c434ec6e330274bcfdb97ee07870ce53
littlefxc <12741723+littlefxc@users.noreply.github.com>
2020-10-14 11:12:09 +0800

    feat(spring-boot-mybatis-kylin):add a new project

---
+++ hive-spring-boot/src/main/java/com/fengxuechao/example/hive/HiveSpringApp.java
@@ -0,0 +1,17 @@
+/*
+ * 版权所有: 爱WiFi无线运营中心
+ * 创建日期: 2020/10/14
+ * 创建作者: 冯雪超
+ * 文件名称: HiveSpringApp.java
+ * 版本: v1.0
+ * 功能:
+ * 修改记录：
+ */
+package com.fengxuechao.example.hive;
+
+/**
+ * @author fengxuechao
+ * @date 2020/10/14
+ */
+public class HiveSpringApp {
+}
