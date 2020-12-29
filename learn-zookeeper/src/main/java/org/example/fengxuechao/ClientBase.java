52e4e638e6af7be084de1e8423efc365bed3c1d7
littlefxc <12741723+littlefxc@users.noreply.github.com>
2020-12-29 19:29:29 +0800

    feat(learn-zookeeper):ZooKeeper 基本的操作示例

---
+++ learn-zookeeper/src/main/java/org/example/fengxuechao/ClientBase.java
@@ -0,0 +1,19 @@
+/*
+ * 版权所有: 爱WiFi无线运营中心
+ * 创建日期: 2020/12/29
+ * 创建作者: 冯雪超
+ * 文件名称: ClientBase.java
+ * 版本: v1.0
+ * 功能:
+ * 修改记录：
+ */
+package org.example.fengxuechao;
+
+/**
+ * @author fengxuechao
+ * @date 2020/12/29
+ */
+public enum ClientBase {
+    ;
+    public static final int CONNECTION_TIMEOUT = 2000;
+}
